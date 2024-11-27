df9a7b1addbda3f6e589dabf8110a90816da0dd5
Eduardo <eduardoarmando.palma@gmail.com>
2024-11-26 23:21:24 -0300

    update script new data legacy foods

--- script/script_food_data.py
+++ script/script_food_data.py
@@ -30,11 +30,12 @@ def translate_single(texts: str) -> str:
 
 
 def convert_food_portions(food_portions):
-    data_dict_portions = [{"name": translate_single(portions.get("measureUnit").get("name")),
-                           "abbreviation": translate_single(portions.get("measureUnit").get("abbreviation")) ,
-                           "grams": float(portions.get("gramWeight")), "amount": float(portions.get("amount"))} for
+    data_dict_portions = [{"name": translate_single(portions.get("modifier")),
+                           "abbreviation": translate_single(portions.get("measureUnit").get("abbreviation")),
+                           "grams": float(portions.get("gramWeight")),
+                           "amount": float(portions.get("amount") if portions.get("amount") is not None else 1.0)} for
                           portions in food_portions]
-
+    print(data_dict_portions)
     return data_dict_portions
 
 
@@ -43,7 +44,7 @@ def transform_data_in_document(data_json, db_instance: Database[Mapping[str, Any
     db_collection_nutrient = db_instance["nutrient_food"]
     db_collection_food = db_instance["food"]
 
-    for food_information in data_json['FoundationFoods']:
+    for food_information in data_json['SRLegacyFoods']:
         name = translate_single(food_information.get("description"))
         food_portions = convert_food_portions(food_information.get("foodPortions"))
         food_category = translate_single(food_information.get("foodCategory").get("description"))
@@ -68,9 +69,8 @@ def transform_data_in_document(data_json, db_instance: Database[Mapping[str, Any
 # nutrient
 if __name__ == '__main__':
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["Food_db"]
-    collection = db["ingredient"]
+    db = client["foods_legacy"]
 
     path = Path(__file__).resolve().parent.parent
-    data = read_json_food_data_central(path / "files_utils/foundationDownload.json")
+    data = read_json_food_data_central(path / "files_utils/FoodData_Central_sr_legacy_food_json_2021-10-28.json") # 4455
     transform_data_in_document(data, db)
